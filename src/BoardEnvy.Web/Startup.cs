443cb8243a6412e9597938cb34a2e46d027d87f1
swizkon <jonas@jerndin.se>
2018-09-11 21:35:14 +0200

    adsdasd

--- src/BoardEnvy.Web/Startup.cs
+++ src/BoardEnvy.Web/Startup.cs
@@ -56,7 +56,7 @@ namespace BoardEnvy.Web
                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                    // .AddEnvironmentVariables()
-                   .AddCommandLine(new string[]{
+                   .AddCommandLine(new string[]{ 
                        "--StorageConnectionString", "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
                    });
             
